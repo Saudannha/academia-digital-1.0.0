53652841959d76f423eb86c2edd5089ab9f5cb29
Saudannha <saudannha@gmail.com>
2022-08-20 13:14:40 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -0,0 +1,31 @@
+package me.dio.academia.digital.entity.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlunoForm {
+
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @Size(min = 3, max = 50, message = "'${ValidateValue}'precisa entrar entre {min} e {max} caracteres.")
+  private String nome;
+
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @CPF(message = "'{validateValue}' é inválido.")
+  private String cpf;
+
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @Size(min = 3, max = 50, message = "'${ValidateValue}'precisa entrar entre {min} e {max} caracteres.")
+  private String bairro;
+
+  @NotNull(message = "Preencha o campo corretamente.")
+  @Past(message = "Data '${validateValue}' é inválida.")
+  private LocalDate dataDeNascimento;
+}
